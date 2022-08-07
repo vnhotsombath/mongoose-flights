63e51c1107693bbf776038cf0c7d95763228e2a1
username <youremail@domain.com>
2022-08-06 21:35:49 -0400

    created destinationSchema

--- models/flight.js
+++ models/flight.js
@@ -29,5 +29,20 @@ const flightSchema = new mongoose.Schema({
     }
 });
 
+//create a destinationSchema 
+const destinationSchema = new mongoose.Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'n/a',
+    },
+    arrival: {
+        type: Date, 
+        default: function() {
+            return new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+        }
+    }
+}); 
+
 // create or model, which will create the collection and return the object that can perform crud operations on that collection (typically use the model in controller files)
 module.exports = mongoose.model('Flight', flightSchema);
