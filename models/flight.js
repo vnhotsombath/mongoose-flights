4436a63f231f1c9606cba12942ff7577afcb5da4
username <youremail@domain.com>
2022-08-08 18:59:10 -0400

    ticketSchema

--- models/flight.js
+++ models/flight.js
@@ -34,7 +34,12 @@ const flightSchema = new Schema({
     timestamps: true,
 })
     
-
+const ticketSchema = new Schema ({
+    seat: {type: String, match: /[A-F][1-9]\d?/},
+    price: {type: Number, min: 0},
+}, {
+    timestamps: true
+})
 
 
 
